74f600eabd342ab692f7a78f94d9110b87caa104
4wayhandshake <timbo.in.space@proton.me>
2024-04-26 14:29:16 +0300

    Added file upload functionality

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # simple-http-server
-A simple extension of Python's http.server module. Serves files in the local directory, but also prints request data to the host.
+A simple extension of Python's http.server module. Serves files in the local directory, but also prints request data to the host. Can also be used for file transfer.
+
+This utility makes an excellent companion to any kind of "blind" web-based attack, including XSS, SQLi, SSRF, etc
 
 ## Instructions
 
@@ -16,6 +18,44 @@ chmod +x SimpleServer.py
 
 Run it, optionally specify a port:
 ```bash
-./SimpleServer.py [port]
+./SimpleServer.py port [-v] [-h]
 ```
 
+> `-v, --verbose` : enables verbose mode. This allows you to see all request headers. 
+> `-h, --help` : show the help text
+
+If you need quick and easy index page, just use **index.html** and **style.css** from this repo.
+
+## Examples
+
+> The following are just normal cURL commands, but might be handy reminders:
+
+**Download** a file adjacent to the server:
+
+```bash
+curl http://127.0.0.1:8080/my_script.js
+```
+
+**Send data** in x-www-urlencoded format:
+
+```bash
+curl -d 'field=value&field2=value2' http://127.0.0.1:8080
+```
+
+**Upload** a file to the server:
+
+```bash
+curl -X POST -F "file=@./beaver.jpg" http://127.0.0.1:8080
+```
+
+
+
+Please :star: this repo if you found it useful!
+
+
+---
+
+Enjoy,
+
+:handshake::handshake::handshake::handshake:
+@4wayhandshake
--- SimpleServer.py
+++ SimpleServer.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
 import argparse
 
 class CustomRequestHandler(BaseHTTPRequestHandler):
-    def _send_response(self, message):
-        self.send_response(200)
+    def _send_response(self, status, message):
+        self.send_response(status)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(message.encode('utf-8'))
-        self.verbose_output()
+        self.wfile.write(message)
 
     def verbose_output(self):
         if not args.verbose:
@@ -30,19 +29,45 @@ class CustomRequestHandler(BaseHTTPRequestHandler):
             self.send_response(500)
         self.end_headers()
         self.wfile.write(bytes(file_to_open, 'utf-8'))
+        self.verbose_output()
+        print("")
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        self._send_response("Received POST request with data:\n{}".format(post_data))
-        if post_data and post_data != '':
-            print(f'\t{post_data}')
+        post_data = self.rfile.read(content_length)
+        content_type = self.headers.get('Content-Type')
+        if content_type and content_type.startswith('multipart/form-data'):
+            try:
+                boundary = content_type.split('; boundary=')[1]
+                parts = post_data.split(boundary.encode())[1:-1]
+                filename = ''
+                for part in parts:
+                    # Extract filename and content from each part
+                    filename = part.split(b'\r\n')[1].split(b';')[-1].split(b'=')[-1].decode().strip('"')
+                    file_content = part.split(b'\r\n\r\n')[1]
+                    # Write file content to a file
+                    with open(filename, 'wb') as f:
+                        f.write(file_content)
+                self._send_response(200, b'File uploaded successfully.')
+                self.verbose_output()
+                print(('\n' if args.verbose else '') + f'\tFile uploaded: {filename}')
+            except Exception as e:
+                self._send_response(500, b'Server error occurred while uploading file.')
+                self.verbose_output()
+                print(f'\tFile failed to upload.')                    
+        else:
+            self._send_response(200, b'POST data received.')
+            self.verbose_output()
+            if post_data and post_data != '':
+                print(('\n' if args.verbose else '') + f'\t{post_data.decode("utf-8")}')
+        print("")
+        
        
 
 def run_server(port=8000):
     server_address = ('', port)
     httpd = HTTPServer(server_address, CustomRequestHandler)
-    print(f"Server is running on port {port}")
+    print(f"Server is running on port {port}\n")
     httpd.serve_forever()
     
 parser = argparse.ArgumentParser(
